d4ce2437a8b5df9e87d4831c52e92558432bfb26
CESAR <calbert2502@gmail.com>
2025-03-01 06:46:25 -0800

    Agregar archivos de proyecto.

---
+++ venta-semilla-de-trigo/Program.cs
@@ -0,0 +1,17 @@
+namespace venta_semilla_de_trigo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Home());
+        }
+    }
+}
---
+++ venta-semilla-de-trigo/Views/Home.cs
@@ -0,0 +1,10 @@
+namespace venta_semilla_de_trigo
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
