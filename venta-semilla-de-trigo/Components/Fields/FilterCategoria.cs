283e3972572e8c3dfaece23c23456bb21d4335ff
CESAR <calbert2502@gmail.com>
2025-03-06 21:35:45 -0800

    Funcionalidad filtros categoria y fecha

--- venta-semilla-de-trigo/Components/Fields/FilterCategoria.cs
+++ venta-semilla-de-trigo/Components/Fields/FilterCategoria.cs
@@ -13,7 +13,10 @@ namespace venta_semilla_de_trigo.Components
 
         public override Func<Venta, bool> GetCondition()
         {
-            throw new NotImplementedException();
+            if (RbTodos.Checked)
+                return v => true;
+
+            return v => v.Basica == RbBasica.Checked;
         }
     }
 }
--- venta-semilla-de-trigo/Components/Fields/FilterDate.cs
+++ venta-semilla-de-trigo/Components/Fields/FilterDate.cs
@@ -13,7 +13,9 @@ namespace venta_semilla_de_trigo.Components
 
         public override Func<Venta, bool> GetCondition()
         {
-            throw new NotImplementedException();
+            var minDate = new DateTime((int)NudMinYear.Value, CbxMinMonth.SelectedIndex + 1, 1);
+            var maxDate = new DateTime((int)NudMaxYear.Value, CbxMaxMonth.SelectedIndex + 1, 1);
+            return v => v.Fecha >= minDate && v.Fecha < maxDate;
         }
     }
 }
