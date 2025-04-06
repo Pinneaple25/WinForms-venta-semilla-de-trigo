a583c5ef4215229b956f60d6e77ff8ac1f0d0f1c
CESAR <calbert2502@gmail.com>
2025-04-05 17:55:52 -0700

    Normalizar campos con espacios extras.

--- venta-semilla-de-trigo/Models/VentaModel.cs
+++ venta-semilla-de-trigo/Models/VentaModel.cs
@@ -5,21 +5,21 @@ namespace venta_semilla_de_trigo.Models
 {
     public class Venta(IExcelDataReader reader)
     {
-        public string Solicitante { get; set; } = reader.GetString(0);
-        public string Variedad { get; set; } = reader.GetString(1);
+        public string Solicitante { get; set; } = reader.GetString(0).Trim();
+        public string Variedad { get; set; } = reader.GetString(1).Trim();
         public bool Basica { get; set; } = reader.GetString(2).StartsWith('B');
         public bool? Duro { get; set; } = reader.GetValue(3)?.ToString()?.StartsWith('D');
         public int KgIncorporacion { get; set; } = (int?)reader.GetValue(4) ?? 0;
         public int KgSalida { get; set; } = (int)reader.GetDouble(5);
         public int Costo { get; set; } = (int)reader.GetDouble(6);
-        public string FolioSalida { get; set; } = reader.GetString(7);
-        public string FolioEntrada { get; set; } = reader.GetString(8);
-        public string Ciclo { get; set; } = reader.GetString(9);
-        public string Lote { get; set; } = reader.GetString(10);
-        public string Etiquetas { get; set; } = reader.GetString(11);
-        public string? Factura { get; set; } = reader.GetValue(12)?.ToString();
+        public string FolioSalida { get; set; } = reader.GetString(7)?.Trim() ?? "";
+        public string FolioEntrada { get; set; } = reader.GetString(8)?.Trim() ?? "";
+        public string Ciclo { get; set; } = reader.GetString(9)?.Trim() ?? "";
+        public string Lote { get; set; } = reader.GetString(10)?.Trim() ?? "";
+        public string Etiquetas { get; set; } = reader.GetString(11)?.Trim() ?? "";
+        public string? Factura { get; set; } = reader.GetValue(12)?.ToString()?.Trim();
         public DateTime Fecha { get; set; } = new DateTime((int)reader.GetDouble(14), DateConverter.MonthNameToInt(reader.GetString(13)), 1);
-        public string Oficio { get; set; } = reader.GetString(15);
-        public string? RegistroSIGI { get; set; } = reader.GetValue(16)?.ToString();
+        public string Oficio { get; set; } = reader.GetString(15)?.Trim() ?? "";
+        public string? RegistroSIGI { get; set; } = reader.GetValue(16)?.ToString()?.Trim();
     }
 }
